435518c0df7724fc76889744677dc03d62e9fa26
Maçana <davirj9@hotmail.com>
2018-10-09 23:18:33 -0300

    Aula1 e Aula2.

--- AulasAvancado/src/main/java/br/com/java/advanced/controller/InicioController.java
+++ AulasAvancado/src/main/java/br/com/java/advanced/controller/InicioController.java
@@ -1,58 +1,27 @@
 package br.com.java.advanced.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
-import javax.inject.Inject;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import br.com.java.advanced.bo.BibliotecaBO;
-import br.com.java.advanced.entity.Biblioteca;
-import br.com.java.advanced.entity.Livro;
-
 /**
  * @author Davi Maçana
  *
  */
-@WebServlet(urlPatterns = "/InicioController",
-			initParams = @WebInitParam(name = "Projeto", value = "Java avançado !"))
+@WebServlet("/inicioController")
 public class InicioController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	
-	@Inject
-    private BibliotecaBO bibliotecaBO;
 	
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		/*response.setContentType("text/html; charset=UTF-8");
-        PrintWriter out = response.getWriter();
-		
-		Biblioteca biblioteca = bibliotecaBO.consultaBiblioteca();
-		
-		out.println("Nome do Projeto: " + getInitParameter("Projeto"));
-		out.println("<table border=1>");
-		
-        for (Livro livro : biblioteca.getLivros()) {
-            out.println("<tr>");
-            out.println("<td>" + livro.getId() + "</td>");
-            out.println("<td>" + livro.getIsbn() + "</td>");
-            out.println("<td>" + livro.getTitulo() + "</td>");
-            out.println("<td>" + livro.getIdioma() + "</td>");
-            out.println("</tr>");
-        }
-        out.println("</table>");
-        out.close();*/
-		
 		request.getRequestDispatcher("/index.jsp").forward(request, response);
-        
 	}
 	
 	@Override
